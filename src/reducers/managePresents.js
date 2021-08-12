27140be2371d491e76b0005630fddedb6499a9fb
grantba <grant.blaire@gmail.com>
2021-08-12 03:38:09 -0500

    commit tree 66ce6520a63560eea88ea13d167ee116b7102c8d

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,28 @@
-export function managePresents(state, action){
+import { defaultConfig } from "sinon";
+
+export function managePresents(state = {numberOfPresents: 0}, action){
+    switch(action.type) {
+        case 'INCREASE':
+            return {...state, numberOfPresents: state.numberOfPresents + 1}
+        case 'DECREASE':
+            return {...state, numberOfPresents: state.numberOfPresents - 1}
+        default:
+            return state;
+    }
 }
+
+  
+// export function managePresents(state = {
+//     numberOfPresents: 0,
+//   }, action) {
+//     switch(action.type) {
+  
+//       case "INCREASE":
+//         return Object.assign({}, state, {
+//           numberOfPresents: state.numberOfPresents + 1
+//         });
+  
+//       default:
+//         return state;
+//     }
+//   };
